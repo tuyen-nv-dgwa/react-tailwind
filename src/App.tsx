6abba0eea99b963839bf0221a03473fe15cd4d79
tuyennv <nguyenvantuyenx2bk@gmail.com>
2023-12-27 13:48:01 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+    <div className=" gap-96 grid-cols-2 flex justify-start">
+      <div className="border-solid border-2 border-indigo-600">01</div>
+      <div className="border-solid border-2 border-indigo-600">02</div>
+      <div className="border-solid border-2 border-indigo-600">03</div>
+      <div className="border-solid border-2 border-indigo-600">04</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
